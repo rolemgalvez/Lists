60ccae7824d791314c0bf42d9468041bb3f2e693
Rolem Galvez <rolemgalvez@gmail.com>
2022-12-26 17:37:24 +0800

    Add project files.

---
+++ Employees/Program.cs
@@ -0,0 +1,54 @@
+﻿//Welcome message
+Console.WriteLine("Employees");
+Console.WriteLine("---------");
+
+//Prepare variables
+bool runAgain;
+string employees = "Juan,Pedro";
+
+do
+{
+    runAgain = false;
+
+
+    //Get user input
+    Console.WriteLine();
+    Console.WriteLine("----------------------");
+    Console.WriteLine($"Current employee list: {employees}");
+    Console.WriteLine("----------------------");
+    Console.Write("Enter first name of the employee to add: ");
+    string? userInput = Console.ReadLine();
+    runAgain = string.IsNullOrEmpty(userInput);
+
+    //Validate input
+    if (runAgain)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Invalid input. Try again.");
+    }
+    else
+    {
+        //Process input
+        List<string> employeeList = employees.Split(",").ToList();
+        employeeList.Add(userInput);
+        employees = employeeList[0];
+        int ctr = 1;
+        while (ctr < employeeList.Count)
+        {
+            employees += $",{employeeList[ctr++]}";
+        }
+
+        //Ask if add again
+        Console.WriteLine("----------------------");
+        Console.WriteLine($"Current employee list: {employees}"); 
+        Console.WriteLine("----------------------");
+        Console.Write("Enter \"yes\" if want to add again: ");
+        runAgain = string.Equals(Console.ReadLine()?.ToLower(), "yes");
+    }
+
+} while (runAgain);
+
+//End of program
+Console.WriteLine();
+Console.WriteLine("End of program.");
+Console.ReadLine();
